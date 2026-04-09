1c2cd530ad2ae4f3ac9e1cafe629f86481530402
Blossom <crimson.rune1467@gmail.com>
2026-04-09 16:57:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.getElementById("gallery");
+    const box = document.querySelector(".box");
+    const colorButton = document.getElementById("changeColor");
+    const toggleButton = document.getElementById("toggleImage");
+
+
+    colorButton.addEventListener("click", () => {
+        box.style.backgroundColor = 
+        box.style.backgroundColor === "blue" ? "red" : "blue";
+    });
+
+    let toggled = false;
+    toggleButton.addEventListener("click", () => {
+        if (toggled) {
+            gallery.src = "./assets/heart.png";
+            toggled = false;
+        } else {
+            gallery.src = "./assets/image/star.png";
+            toggled = true;
+        }
+    });
+
+    console.log("Page loaded!")
+  });
